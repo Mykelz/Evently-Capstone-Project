34db3e59bd01f020f9299c6b649ea4b5d69a95e4
Obianuka micheal <obianukamicheal@gmail.com>
2024-08-17 00:55:44 +0100

    rate limit

--- app.js
+++ app.js
@@ -6,11 +6,22 @@ const eventRoute = require('./routes/events');
 const ticketRoute = require('./routes/event-ticket');
 const scheduleReminders = require('./utils/reminder');
 const schedule = require('node-schedule')
+const { rateLimit } = require('express-rate-limit');
 
 const app = express();
 
-app.use(express.json());
+const limiter = rateLimit({
+	windowMs: 0.5 * 60 * 1000, // 15 minutes
+	limit: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
 
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
+
+app.use(express.json());
 
 app.use('/auth', userRoute);
 app.use(eventRoute);
@@ -27,14 +38,5 @@ app.use((error, req, res, next) =>{
     res.status(status).json({ message: message, data: data})
   })
 
-
-mongoose.connect(process.env.CONNECT).then(connection=>{
-    console.log('Database connected')
-    app.listen(process.env.PORT || 4000, ()=>{
-        console.log(`app is listening on port ${process.env.PORT}`)
-    })
-}).catch(err=>{
-    console.log(err)
-})
 
 module.exports = app;
