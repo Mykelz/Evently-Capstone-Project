856518c5a31a1c27195a2d949a55fbdfff75dd3b
Obianuka micheal <obianukamicheal@gmail.com>
2024-08-26 20:33:53 +0100

    new commit

--- utils/redis.js
+++ utils/redis.js
@@ -1,8 +1,8 @@
 const redis = require('redis');
 
 const client = redis.createClient({
-    host: process.env.REDIS_URL,
-})
+    url: process.env.REDIS_URL
+});
 
 
 client.on("connect", () => console.log("redis client is connected"));
